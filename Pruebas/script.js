eca9c69bd726446dd07831af93682250d43b33ea
psanroj268 <psanroj268@g.educaand.es>
2022-09-21 09:20:43 +0200

    Add Files

---
+++ Pruebas/script.js
@@ -0,0 +1,7 @@
+//alert("Hola! Soy una caja de alerta")            
+//(Obtienes una caja de alerta)
+
+/*function muestraAlerta(){
+    alert("Hola!")
+}
+(Obtienes una caja de alerta cuando pulsas en un parrafo)*/
