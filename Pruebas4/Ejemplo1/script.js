aad7820e8695f672903daa6aa1526b48b9adb4c9
psanroj268 <psanroj268@g.educaand.es>
2022-10-05 09:00:59 +0200

    Update Files

--- Pruebas4/Ejemplo1/script.js
+++ Pruebas4/Ejemplo1/script.js
@@ -1,8 +1,32 @@
 "use strict"
 
-let persona;
+let valor1 = document.getElementById("operador1");
+let valor2 = document.getElementById("operador2");
 
-function saluda() {
-    persona = prompt("Dime tu nombre: ", "Pablo")
-    alert("Hola! " + persona);
+let suma = document.getElementById("boton1");
+let resta = document.getElementById("boton2");
+let multiplica = document.getElementById("boton3");
+let divide = document.getElementById("boton4");
+let modulo = document.getElementById("boton5");
+
+let resultado = document.getElementById("salida");
+
+suma.onclick = function(){
+    resultado.value = parseFloat(valor1.value) + parseFloat(valor2.value);
+}
+
+resta.onclick = function(){
+    resultado.value = parseFloat(valor1.value) - parseFloat(valor2.value);
+}
+
+multiplica.onclick = function(){
+    resultado.value = parseFloat(valor1.value) * parseFloat(valor2.value);
+}
+
+divide.onclick = function(){
+    resultado.value = parseFloat(valor1.value) / parseFloat(valor2.value);
+}
+
+modulo.onclick = function(){
+    resultado.value = parseFloat(valor1.value) % parseFloat(valor2.value);
 }
