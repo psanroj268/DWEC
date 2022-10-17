0a7806c99e8500b58223c5c1a2f33994166c66da
psanroj268 <psanroj268@g.educaand.es>
2022-10-17 09:32:20 +0200

    Update Files

--- Pruebas5/Ejemplo1/script.js
+++ Pruebas5/Ejemplo1/script.js
@@ -19,5 +19,65 @@ let rapido = document.getElementById("boton13");
 let lento = document.getElementById("boton14");
 
 mayusculas.onclick = function(){
-    frase1.value = frase1.toUpperCase();
+    frase1.value = frase1.value.toUpperCase();
+}
+
+minusculas.onclick = function(){
+    frase1.value = frase1.value.toLowerCase();
+}
+
+priMayus.onclick = function(){
+    const palabras = frase1.value.split(' ');
+
+    for (let i = 0; i < palabras.length; i++) {
+        palabras[i] = palabras[i][0].toUpperCase() + palabras[i].substr(1);
+    }
+
+    frase1.value = palabras.join(' ');
+}
+
+ultMayus.onclick = function (){
+    frase1.value = frase1.value.split(" ").map(function (l){
+        return l.slice(0, -1) + l.slice(-1).toUpperCase();
+    }).join(" ");
+}
+
+priMinus.onclick = function(){
+    const palabras = frase1.value.split(' ');
+
+    for (let i = 0; i < palabras.length; i++) {
+        palabras[i] = palabras[i][0].toLowerCase() + palabras[i].substr(1);
+    }
+
+    frase1.value = palabras.join(' ');
+}
+
+ultMinus.onclick = function (){
+    frase1.value = frase1.value.split(" ").map(function (l){
+        return l.slice(0, -1) + l.slice(-1).toLowerCase();
+    }).join(" ");
+}
+
+vocMayus.onclick = function (){
+    frase1.value = frase1.value.replace(/[aeiou]/g, function (l){
+        return l.toUpperCase();
+    });
+}
+
+vocMinus.onclick = function (){
+    frase1.value = frase1.value.replace(/[AEIOU]/g, function (l){
+        return l.toLowerCase();
+    });
+}
+
+conMayus.onclick = function (){
+    frase1.value = frase1.value.replace(/[bcdfghjklmnñpqrstvwxyz]/g, function (l){
+        return l.toUpperCase();
+    });
+}
+
+conMinus.onclick = function (){
+    frase1.value = frase1.value.replace(/[BCDFGHJKLMNÑPQRSTVWXYZ]/g, function (l){
+        return l.toLowerCase();
+    });
 }
